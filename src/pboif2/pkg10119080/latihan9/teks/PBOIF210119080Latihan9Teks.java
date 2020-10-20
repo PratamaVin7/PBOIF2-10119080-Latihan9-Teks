14efbf0342d4ce74f58664ab43152290f0badecd
unknown <kevinpratamaoke@gmail.com>
2020-10-20 16:25:51 +0700

    First commit

---
+++ src/pboif2/pkg10119080/latihan9/teks/PBOIF210119080Latihan9Teks.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119080.latihan9.teks;
+
+/**
+ *
+ * @author
+ * NAMA     : Kevin Pratama Sinaga
+ * KELAS    : PBOIF2
+ * NIM      : 10119080
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+public class PBOIF210119080Latihan9Teks {
+
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
